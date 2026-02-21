8d4d7a3daea755047fb397693b62b7b2955c1bd5
Sithum Bopitiya <sithumsandeepap@gmail.com>
2026-02-21 14:02:55 +0530

    Fix asssets export

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,11 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
+  const isProduction = mode === 'production';
+
   return {
+    base: isProduction ? '/payment-reconciler/' : '/',
     plugins: [react(), tailwindcss()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
